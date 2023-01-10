cd2fd2b86b08f92b7f1477daa6d67b0c9518fa50
Hannes K <101644829+Hansk1@users.noreply.github.com>
2023-01-11 00:01:40 +0200

    Add files via upload

---
+++ BubbleSortBetter.py
@@ -0,0 +1,45 @@
+import random
+import time
+
+#from BubbleSort import List
+
+#Funktio randomisoidun listan luontiin:
+def CreateList(i):
+    RandomizedList = []
+    for l in range(i):
+        n = random.randint(1, 100)
+        RandomizedList.append(n)
+    return RandomizedList
+        
+       
+#Funktio listan numeroiden lajitteluun BubbleSort algoritmilla:
+def BubbleSort(List):
+    n = len(List)
+    switch = False
+    
+    #Käydään listan elementit läpi
+    # -1 Lisätään, jotta vältytään yhdeltä ylimääräiseltä läpikäyntikerralta
+    for j in range(n-1):
+        for i in range(n-j-1):
+            #Vaihdetaan elementtien paikat, jos tarkasteltava elementti on isompi kuin seuraava elementti:
+            if (List[i] > List[i + 1]):
+                switch = True
+                List[i], List[i + 1] = List[i + 1], List[i]
+                #Tulostetaan lista jokaisen iteraation jälkeen.
+                #print(List)
+        #Jos listalle ei tarvitse tehdä yhtään vaihtoa, niin poistutaan loopista suoraan.
+        if not switch:
+            return
+
+#Luodaan lajiteltava lista ja tulostetaan se käyttäjälle: 
+List = CreateList(10000)
+print("Randomisoitu lista on:\n" + str(List))
+
+#Aloitetaan ajastin BubbleSort algoritmille:
+StartTime = time.time() 
+
+#Lajitellaan lista ja tulostetaan se käyttäjälle kuluneen ajan kanssa:
+BubbleSort(List)
+
+print("\nLajiteltu lista on:\n" + str(List))
+print("Kulunut aika:", round(time.time()-StartTime, 2), "s.")
